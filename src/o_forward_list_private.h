76c017f30749700a4c4777d3f3c6ea01b79e2f00
Safety-Override <88028794+Safety-Override@users.noreply.github.com>
2021-11-11 22:12:45 +0300

    copyrighy

---
+++ src/o_forward_list_private.h
@@ -0,0 +1,18 @@
+/** 
+ * Copyright (c) 2021 Safety-Override
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT license. See LICENSE for details.
+ */
+
+#include <stdint.h>
+
+struct o_forward_list_node_s {
+    struct o_forward_list_node_s* next;
+    char data[0];
+};
+
+struct o_forward_list_s {
+    struct o_forward_list_node_s* front;
+    size_t data_type_size;
+};
