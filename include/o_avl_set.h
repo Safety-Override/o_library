398d5a4ecda828536022c6693581d04357ab2666
Safety-Override <igoryagushin@gmail.com>
2022-03-19 04:54:30 +0300

    c

--- include/o_avl_set.h
+++ include/o_avl_set.h
@@ -1,3 +1,10 @@
+/** 
+ * Copyright (c) 2021 Safety-Override
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT license. See LICENSE for details.
+ */
+
 #include <stddef.h>
 #include "o_functions.h"
 
