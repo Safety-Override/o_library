b71f767f4241666d1545eee6b95c36952975dd8d
Safety-Override <igoryagushin@gmail.com>
2022-04-22 21:39:06 +0300

    container updated

--- src/o_container_private.h
+++ src/o_container_private.h
@@ -5,6 +5,11 @@
  * under the terms of the MIT license. See LICENSE for details.
  */
 
+#ifndef O_CONTAINER_PRIVATE_H_
+#define O_CONTAINER_PRIVATE_H_
+
 struct o_container_node_s {
     char data[0];
 };
+
+#endif  // O_CONTAINER_PRIVATE_H_
