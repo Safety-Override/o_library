c1be428c2c45c57c0d5a78b5a69f96f7be7b759f
Safety-Override <igoryagushin@gmail.com>
2022-04-22 21:37:09 +0300

    forward list private update

--- src/o_forward_list_private.h
+++ src/o_forward_list_private.h
@@ -5,14 +5,20 @@
  * under the terms of the MIT license. See LICENSE for details.
  */
 
+#ifndef O_FORWARD_LIST_PRIVATE_H_
+#define O_FORWARD_LIST_PRIVATE_H_
+
 #include <stddef.h>
 
 struct o_forward_list_node_s {
     struct o_forward_list_node_s* next;
-    char data[0];
 };
 
 struct o_forward_list_s {
     struct o_forward_list_node_s* front;
-    size_t data_type_size;
+    size_t node_size;
+    size_t offsetof_value;
+    size_t sizeof_value;
 };
+
+#endif  // O_FORWARD_LIST_PRIVATE_H_
